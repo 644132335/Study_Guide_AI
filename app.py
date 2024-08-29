130ccc3e710eb98c8772c06e6f224c543098c063
Ray <rayzhang1026@gmail.com>
2024-08-29 01:36:17 -0500

    add the functionality for uploading an image to get questions/answers

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-import json
-import os
-import pathlib
-import textwrap
 from typing import Dict, List
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from PIL import Image
 from pydantic import BaseModel
 
 from gemini_generate import getContentFromAI
@@ -38,16 +35,43 @@ def home():
 
 
 @app.route("/getContent", methods=["POST"])
-def handle_form_submission() -> tuple:
+def handle_form_submission_text() -> tuple:
     data = request.get_json()
     number = data.get("number")
     text = data.get("text")
     base_prompt_for_qa = f"I will give you a passage in the delimiter. First give a summary to the passage. Then, try to generate {number} questions that test readers' understanding of the passage and provide solutions."
     optimized_qa_prompt = (
         base_prompt_for_qa
-        + f"Please provide a response in a structured JSON format that matches the following model: {json_model}"
+        + f" Please provide a response in a structured JSON format that matches the following model: {json_model}"
     )
     generatedQA = getContentFromAI(optimized_qa_prompt + f"'''{text}'''")
+    extractJson = extract_json(generatedQA)
+    try:
+        data = jsonify(extractJson)
+        return data, 200
+    except Exception as e:
+        data = jsonify({"error": str(e)})
+        return data, 500
+
+
+@app.route("/getContentByImage", methods=["POST"])
+def handle_form_submission_image() -> tuple:
+    """
+    This function is responsible for handling the form submission for the image.
+    Supported file types are: png, jpeg, webp, heic, heif
+    """
+    image = request.files["image"]
+    number = request.form["number"]
+
+    base_prompt_for_qa = f"I will give you a passage in the image. First give a summary to the passage. Then, try to generate {number} questions that test readers' understanding of the passage and provide solutions."
+    optimized_qa_prompt = (
+        base_prompt_for_qa
+        + f" Please provide a response in a structured JSON format that matches the following model: {json_model}"
+    )
+    image = Image.open(
+        image.stream
+    )  # convert the filestorage object to an PIL image object
+    generatedQA = getContentFromAI(optimized_qa_prompt, img=image)
     extractJson = extract_json(generatedQA)
     try:
         data = jsonify(extractJson)
--- gemini_generate.py
+++ gemini_generate.py
@@ -6,33 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_DANGEROUS",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-]
-
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
 
 
-def getContentFromAI(text: str) -> str:
-    request = model.generate_content(text, safety_settings=safety_settings)
+def getContentFromAI(text: str, img=None) -> str:
+    if img:
+        request = model.generate_content([img, text])
+    else:
+        request = model.generate_content(text)
     return request.text
